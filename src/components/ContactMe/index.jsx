3cbc14ab7e9a419c6f96016e6c4f79aa88e64e3b
Luis Medina <lamc3005@gmail.com>
2022-11-04 10:42:20 -0400

    add package formik and use it in the form of contact

--- src/components/ContactMe/index.jsx
+++ src/components/ContactMe/index.jsx
@@ -3,15 +3,9 @@ import { BiTimeFive } from 'react-icons/bi'
 import { BsFillTelephoneFill } from 'react-icons/bs'
 import { ImLocation2 } from 'react-icons/im'
 import './contactMeStyles.css'
-
+import { Formik } from 'formik'
 function ContactMe() {
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const name = e.target.first.value
-    const last = e.target.last.value
-    return { name, last }
-  }
   return (
     <section id="contactMe">
       <div className="contactMe__container">
@@ -20,18 +14,102 @@ function ContactMe() {
           <hr />
           <p>You can contact us any way that is convenient for you. We are available 24/7 via fax or email.
             You can also use a quick contact form below or visit our salon personally.</p>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <input type="text" name="first" id="first" placeholder="First name" />
-              <input type="text" name="last" id="last" placeholder="Last name" />
-            </div>
-            <div>
-              <input type="number" name="phone" id="phone" placeholder="Phone" />
-              <input type="email" name="email" id="email" placeholder='Email' />
-            </div>
-            <textarea name="text" id="text" cols="30" rows="10"></textarea>
-            <button type="submit">Send Message</button>
-          </form>
+          <Formik
+            initialValues={{
+              email: '',
+              first: '',
+              last: '',
+              phone: '',
+              text: ''
+            }}
+            validate={values => {
+              const errors = {};
+              if (!values.email) {
+                errors.email = <p className='text__error'>The email is required</p>;
+              } else if (
+                !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(values.email)
+              ) {
+                errors.email = <p styles={{ color: red }}>This email address is invalid</p>;
+              }
+              return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+              }, 400);
+            }}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+            }) => (
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <input
+                    type="text"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    name="first"
+                    value={values.first}
+                    placeholder="First name"
+                  />
+                  <input
+                    type="text"
+                    name="last"
+                    value={values.last}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    placeholder="Last name"
+                  />
+                </div>
+
+                <div>
+                  <input
+                    type="number"
+                    name="phone"
+                    id="phone"
+                    value={values.phone}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    placeholder="Phone"
+                  />
+                  <input
+                    type="email"
+                    name="email"
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    id="email"
+                    placeholder='Email'
+                  />
+
+                </div>
+                <textarea
+                  name="text"
+                  id="text"
+                  cols="30"
+                  rows="10"
+                  value={values.text}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                >
+                </textarea>
+                <button type="submit" disabled={isSubmitting}>
+                  Send Message
+                </button>
+                <div className='container__errors'>
+
+                  {errors.email && touched.email && errors.email}
+                </div>
+              </form>
+            )}
+          </Formik>
         </div>
         <article className='contactMe__information'>
           <div>
