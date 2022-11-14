d01a5c78e987945a8c1fe0be2b6c1d1709d5ef8a
Luis Medina <lamc3005@gmail.com>
2022-11-14 17:14:55 -0400

    fix navbar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,12 +3,12 @@ import './headerStyles.css'
 function Header() {
   return (
     <header className='header'>
-      <nav>
-        <ul>
-          <li><a href="#home">Janef Nails</a></li>
-          <li><a href="#aboutMe">About</a></li>
-          <li><a href="#contactMe">Contact</a></li>
-        </ul>
+      <nav className='navigation'>
+        <a href="#home">Janef Nails</a>
+        <div>
+          <a href="#aboutMe">About</a>
+          <a href="#contactMe">Contact</a>
+        </div>
       </nav>
     </header>
   )
