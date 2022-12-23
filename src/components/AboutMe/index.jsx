80ad54a13cb378763fd2cdbe8bf60c95710e853a
Luis Medina <lamc3005@gmail.com>
2022-12-23 10:18:50 -0400

    add services

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -1,26 +1,39 @@
 import './aboutMeStyles.css'
-import Carrusel from '../Carusel'
+import Carrusel from '../Carrusel'
 
 function AboutMe() {
-
-  return (
-    <section className='aboutMe' id='aboutMe'>
-      <div className="aboutMe__container">
-        <div className='aboutMe__containerText'>
-          <h2>Take time for yourself today</h2>
-          <p>Amet justo donec enim diam vulpultate ut pharetra. Diam in arcu cursus euimod</p>
-          <ul>
-            <li>Toxins free</li>
-            <li>Try new foundation</li>
-            <li>Shine line a diamond</li>
-          </ul>
-        </div>
-        <div className='aboutMe_containerCarrusel'>
-          <Carrusel />
-        </div>
-      </div>
-    </section>
-  )
+	return (
+		<section className='aboutMe' id='aboutMe'>
+			<div className='aboutMe__container'>
+				<div className='aboutMe__containerText'>
+					<h2>Toma tiempo para ti misma hoy</h2>
+					<p>Servicios:</p>
+					<div className='services'>
+						<p>MANICURA RUSSA:</p>
+						<ul>
+							<li>Manicura con esmaltado semipermanente.</li>
+							<li>Manicura con base rubber.</li>
+						</ul>
+					</div>
+					<div className='services'>
+						<p>PEDICURA:</p>
+						<ul>
+							<li>Pedicura con esmaltado semipermanente</li>
+						</ul>
+					</div>
+					<div className='services'>
+						<p>SISTEMAS:</p>
+						<ul>
+							<li>Sistema acrílico</li>
+						</ul>
+					</div>
+				</div>
+				<div className='aboutMe_containerCarrusel'>
+					<Carrusel />
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default AboutMe
