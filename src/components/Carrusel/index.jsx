b9303560107370857bd1a66bf3c99c0e7484e9dc
Luis Medina <lamc3005@gmail.com>
2022-12-23 10:18:32 -0400

    change carrusel, using swiper

---
+++ src/components/Carrusel/index.jsx
@@ -0,0 +1,49 @@
+import img2 from '../../assets/second.webp'
+import img3 from '../../assets/tercero.webp'
+import img4 from '../../assets/cuarto.webp'
+import img5 from '../../assets/quinta.webp'
+import img6 from '../../assets/sexta.webp'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { EffectCube, Pagination } from 'swiper'
+import './carrusel.css'
+import 'swiper/css'
+import 'swiper/css'
+import 'swiper/css/effect-cube'
+import 'swiper/css/pagination'
+
+function Carrusel() {
+	return (
+		<Swiper
+			spaceBetween={20}
+			slidesPerView={1}
+			className='carrusel'
+			effect={'cube'}
+			grabCursor={true}
+			cubeEffect={{
+				shadow: true,
+				slideShadows: true,
+				shadowOffset: 20,
+				shadowScale: 1.2,
+			}}
+			pagination={true}
+			modules={[EffectCube, Pagination]}>
+			<SwiperSlide>
+				<img src={img2} alt='img 2' className='img__carrusel' />
+			</SwiperSlide>
+			<SwiperSlide>
+				<img src={img3} alt='img 3' className='img__carrusel' />
+			</SwiperSlide>
+			<SwiperSlide>
+				<img src={img4} alt='img 4' className='img__carrusel' />
+			</SwiperSlide>
+			<SwiperSlide>
+				<img src={img5} alt='img 5' className='img__carrusel' />
+			</SwiperSlide>
+			<SwiperSlide>
+				<img src={img6} alt='img 6' className='img__carrusel' />
+			</SwiperSlide>
+		</Swiper>
+	)
+}
+
+export default Carrusel
