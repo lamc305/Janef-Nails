f05335c34a03341696753149545291ee3d9b5f88
Luis Medina <lamc3005@gmail.com>
2022-12-23 10:18:06 -0400

    change color and the text

--- src/components/Portada/index.jsx
+++ src/components/Portada/index.jsx
@@ -2,24 +2,34 @@ import './portadaStyles.css'
 import hero from '../../assets/LogoJanef.webp'
 
 function Portada() {
-  return (
-    <section className="Portada" id='home'>
-      <div className='portada__container'>
-        <div className='portada__containerText'>
-          <h1>Janetzi Franco</h1>
-          <h2>Nail's Art</h2>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti ea dolor doloremque cumque nostrum, magni esse. Nostrum, dicta necessitatibus ex sapiente quos animi impedit odit earum cupiditate ad suscipit? Dolore!</p>
-          <button>
-            <p>Join now</p>
-          </button>
-        </div>
-        <div className='container__hero'>
-          <img className='portada__hero' src={hero} alt="logo-hero" />
-        </div>
-      </div>
-      <div className="wave"></div>
-    </section>
-  )
+	return (
+		<section className='Portada' id='home'>
+			<div className='portada__container'>
+				<div className='portada__containerText'>
+					<h1>Janetzi Franco</h1>
+					<h2>Nail's Art</h2>
+					<p>
+						Janef Nails tiene como propósito resaltar la belleza de tus uñas
+						convirtiéndolas en arte.
+					</p>
+					<p>
+						Recuerda: ¡Cuidar de ti misma no es un gasto, es una inversión!
+						Aprovecha las oportunidades, consiéntete, amáte y comienza el día
+						con una linda manicura
+					</p>
+					<a
+						href='https://api.whatsapp.com/send/?phone=584246923284'
+						target='_blank'>
+						<p>Join now</p>
+					</a>
+				</div>
+				<div className='container__hero'>
+					<img className='portada__hero' src={hero} alt='logo-hero' />
+				</div>
+			</div>
+			<div className='wave'></div>
+		</section>
+	)
 }
 
 export default Portada
