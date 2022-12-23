d4b24d1e83f7c377fc9ac3471e9356d983ee7884
Luis Medina <lamc3005@gmail.com>
2022-12-23 10:20:14 -0400

    change background color

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,33 @@
+import { useEffect } from 'react'
+import { useState } from 'react'
 import './headerStyles.css'
 
 function Header() {
-  return (
-    <header className='header'>
-      <nav className='navigation'>
-        <a href="#home">Janef Nails</a>
-        <div>
-          <a href="#aboutMe">About</a>
-          <a href="#contactMe">Contact</a>
-        </div>
-      </nav>
-    </header>
-  )
+	const [show, setShow] = useState(false)
+	const transtionNavBar = () => {
+		if (window.scrollY <= 100) {
+			setShow(true)
+		} else {
+			setShow(false)
+		}
+	}
+
+	useEffect(() => {
+		window.addEventListener('scroll', transtionNavBar)
+		return () => window.removeEventListener('scroll', transtionNavBar)
+	}, [])
+
+	return (
+		<header className={`header ${show && 'hidden'}`}>
+			<nav className='navigation'>
+				<a href='#home'>Janef Nails</a>
+				<div>
+					<a href='#aboutMe'>About</a>
+					<a href='#contactMe'>Contact</a>
+				</div>
+			</nav>
+		</header>
+	)
 }
 
-export default Header 
+export default Header
